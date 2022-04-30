530dac3d793c4ed72d02b3d58bd828798b8b4f72
AQW <alice@valour.me>
2022-04-30 13:18:40 -0400

    end first datavis session

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import P from 'papaparse';
 
-import CostChart, {CostEntryArray} from './Cost';
+import CostChart, { CostEntryArray } from './Cost';
 
 import './App.css';
 
 
-class App extends React.Component<{}, {cost: CostEntryArray[] }> {
-  
-  constructor(props : any) {
+class App extends React.Component<{}, { cost: CostEntryArray[] }> {
+
+  constructor(props: any) {
     super(props);
     this.state = { cost: [] };
   }
-  
-  componentDidMount(){
-    P.parse('cost.csv', {download: true, complete: this.onCostParseComplete});
+
+  componentDidMount() {
+    P.parse('cost.csv', { download: true, complete: this.onCostParseComplete });
   }
 
-  onCostParseComplete = (results: any, file : any) => {
+  onCostParseComplete = (results: any, file: any) => {
     results.data.shift();
-    if(results.errors.length === 0){
-      const cost = results.data.map(([date, corn , soy, wheat] : [string, string, string, string]) => 
-         ([new Date(date), parseFloat(corn), parseFloat(soy), parseFloat(wheat)])
+    if (results.errors.length === 0) {
+      // TODO instead of taking the last 100 entries, we want to get an average over the months
+      const data = results.data.splice(-100);
+      const cost = data.map(([date, corn, soy, wheat]: [string, string, string, string]) =>
+        ([date, parseFloat(corn), parseFloat(soy), parseFloat(wheat)])
       );
       this.setState({ cost })
     }
--- src/Cost.tsx
+++ src/Cost.tsx
@@ -1,35 +1,68 @@
 import React from 'react'
 import { Line, ResponsiveLine } from '@nivo/line'
 
-export type CostEntryArray = [Date, number, number, number];
+export type CostEntryArray = [string, number, number, number];
 
 type DataPoint = {
-  x: Date,
+  x: string,
   y: number
 }
 
-const CostChart : React.FC<{cost: CostEntryArray[]}> = ({cost}) => {
-/*
-  const cornData : DataPoint[] = [];
-  const soyData : DataPoint[] = [];
-  const wheatData : DataPoint[] = [];
+const CostChart: React.FC<{ cost: CostEntryArray[] }> = ({ cost }) => {
+  const cornData: DataPoint[] = [];
+  const soyData: DataPoint[] = [];
+  const wheatData: DataPoint[] = [];
 
-  cost.forEach(([date, corn , soy, wheat] : CostEntryArray) => {
-    cornData.push({x: date, y: corn});
-    soyData.push({x: date, y: soy});
-    wheatData.push({x: date, y: wheat});
+  cost.forEach(([date, corn, soy, wheat]: CostEntryArray) => {
+    cornData.push({ x: date, y: corn });
+    soyData.push({ x: date, y: soy });
+    wheatData.push({ x: date, y: wheat });
   });
-*/
+
   const data = [
-    { id: "corn", color: "yellow" , data: [{x:0, y: 1},{ x:1, y: 2}] },
-    { id: "soy", color: "green" , data: [{x:0, y: 3},{ x:1, y: 8}] },
-    {id: "wheat", color: "brown", data: [{x:0, y: 6},{ x:1, y: 10}] }
+    { id: "corn", data: cornData },
+    { id: "soy", data: soyData },
+    { id: "wheat", data: wheatData }
   ];
-
+  /*
+    const x1 = `2001-01-01`;
+    const x2 = `2001-01-02`;
+    const data = [
+      { id: "corn", color: "yellow", data: [{ x: x1, y: 1 }, { x: x2, y: 2 }] },
+      { id: "soy", color: "green", data: [{ x: x1, y: 3 }, { x: x2, y: 8 }] },
+      { id: "wheat", color: "brown", data: [{ x: x1, y: 6 }, { x: x2, y: 10 }] }
+    ];
+  */
   // legends={[{itemWidth: 60, itemHeight: 20}]} 
+  //{(date: Date) => date.toLocaleDateString() }
 
-  return (<div style={{width: "100%", height: "100vh"}}>
-    <ResponsiveLine data={data} />
+  const dateFormat = "%Y-%m-%d";
+  return (<div style={{ width: "100%", height: "100vh" }}>
+    <ResponsiveLine data={data}
+      pointLabelYOffset={5}
+      xFormat={`time:${dateFormat}`}
+      xScale={{
+        type: 'time',
+        format: dateFormat,
+        useUTC: false,
+        precision: 'day',
+      }}
+      // yScale={{
+      //   type: 'linear',
+      //   max: 20
+      // }}
+      margin={{
+        bottom: 50,
+        left: 50,
+        right: 50,
+        top: 20
+      }}
+      axisBottom={{
+        format: '%b %d',
+      }}
+      enableArea={true}
+      enablePoints={false}
+    />
   </div>)
 };
 
